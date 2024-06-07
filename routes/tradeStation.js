6b248f54d3e04548e7741c5e497d43a11ac5d572
Rong Ling <wumnlr@gmail.com>
2024-06-07 15:11:02 -0700

    tradestation

---
+++ routes/tradeStation.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const PORT = 5000
+
+const app = express()
+
+// Enable cors
+app.use(cors())
+
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+app.use(bodyParser.json());
+// Set static folder
+// app.use(express.static('public'))
+
+// Routes
+app.use('/tradeStationApi', require('./routes/tradeStation'))
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
